1bcf1a8642020033963d2996778c0c0b60270ccd
luciakratz-arch <luciakratz@gmail.com>
2026-07-01 00:09:36 -0300

    Reorder import statements in main.py

--- functions/main.py
+++ functions/main.py
@@ -11,6 +11,7 @@ Deploy individual:
   firebase deploy --only functions:webhookCreditos
 """
 
-from webhook_mercadopago import webhookMercadoPago   # noqa: F401
-from webhook_hotmart import webhookHotmart            # noqa: F401
-from webhook_creditos import webhookCreditos          # noqa: F401
+from webhook_mercadopago import webhookMercadoPago          # noqa: F401
+from webhook_hotmart import webhookHotmart                  # noqa: F401
+from webhook_creditos import webhookCreditos                # noqa: F401
+from gerador_cultura_function import gerarLaudoCultura     # noqa: F401
